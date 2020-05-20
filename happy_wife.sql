731ca416583383bcf300af591ea5649c2c1ea438
kyle-deleongit config --global user.email kydeleon10@gmail.comgit config --global --listgit config --global user.name kyle-deleon <kydeleon10@gmail.com>
2020-05-19 22:00:09 -0700

    added marriage_title,account, partners, and partnership

---
+++ happy_wife.sql
@@ -0,0 +1,91 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema happy_wife
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `happy_wife` ;
+
+-- -----------------------------------------------------
+-- Schema happy_wife
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `happy_wife` DEFAULT CHARACTER SET utf8 ;
+USE `happy_wife` ;
+
+-- -----------------------------------------------------
+-- Table `happy_wife`.`users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `happy_wife`.`users` ;
+
+CREATE TABLE IF NOT EXISTS `happy_wife`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(255) NULL DEFAULT NULL,
+  `last_name` VARCHAR(255) NULL DEFAULT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `password` VARCHAR(255) NULL DEFAULT NULL,
+  `husband` TINYINT(1) NULL DEFAULT NULL,
+  `wife` TINYINT(1) NULL DEFAULT NULL,
+  `partnered` TINYINT(1) NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `happy_wife`.`partnerships`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `happy_wife`.`partnerships` ;
+
+CREATE TABLE IF NOT EXISTS `happy_wife`.`partnerships` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `partner_id` INT NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_partners_users_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_partners_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `happy_wife`.`users` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `happy_wife`.`tasks`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `happy_wife`.`tasks` ;
+
+CREATE TABLE IF NOT EXISTS `happy_wife`.`tasks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `partnership_id` INT NOT NULL,
+  `created_by_id` INT NULL DEFAULT NULL,
+  `description` VARCHAR(255) NULL DEFAULT NULL,
+  `value` VARCHAR(255) NULL DEFAULT NULL,
+  `for_id` INT NULL,
+  `completed_by_id` INT NULL DEFAULT NULL,
+  `completed` TINYINT(1) NULL DEFAULT NULL,
+  `approved` TINYINT(1) NULL DEFAULT NULL,
+  `task` TINYINT(1) NULL DEFAULT NULL,
+  `reward` TINYINT(1) NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_tasks_partners1_idx` (`partnership_id` ASC) VISIBLE,
+  CONSTRAINT `fk_tasks_partners1`
+    FOREIGN KEY (`partnership_id`)
+    REFERENCES `happy_wife`.`partnerships` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- server.py
+++ server.py
@@ -55,7 +55,7 @@ def on_register():
         flash("Passwords must match")
 
     if is_valid:
-        query = "INSERT INTO users (first_name, last_name, email, wife, husband, password, created_at, updated_at) VALUES ( %(fn)s, %(ln)s, %(em)s, %(pw)s, NOW(), NOW())"
+        query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES ( %(fn)s, %(ln)s, %(em)s, %(pw)s, NOW(), NOW())"
         data = {
             "fn": request.form['fn'],
             "ln": request.form['ln'],
@@ -95,7 +95,7 @@ def on_login():
             else:
                 session['user_id'] = result[0]['id']
                 session['name'] = result[0]['first_name']
-                return redirect('/home')
+                return redirect('/account')
         else:
             flash("incorrect email and/or password")
 
@@ -107,14 +107,106 @@ def on_logout():
     return redirect('/')
 
 @app.route('/title')
-def become_admin():
+def marriage_title():
     if "user_id" not in session:
         return redirect('/')
     query = "SELECT * FROM users WHERE id = %(sid)s"
     data = {'sid': session['user_id']}
     mysql = connectToMySQL(schema)
     users = mysql.query_db(query,data)
-    return render_template("title.html", users = users)
+    return render_template("marriage_title.html", users = users)
+
+@app.route('/wife_click/<user_id>', methods = ["POST"])
+def wife_click(user_id):
+    if "user_id" not in session:
+        return redirect('/')
+
+    #is_valid = True
+    #if request.form['age'] < 23:
+        #is_valid = False
+        #flash("Admins must be 24 years old or older")
+    #if is_valid:
+    query = "UPDATE users SET husband = false, wife = true, partnered = false, updated_at = NOW() WHERE id = %(uid)s"
+    data = {'uid':user_id}
+    mysql = connectToMySQL(schema)
+    mysql.query_db(query, data)
+
+    return redirect ('/partners')
+
+@app.route('/husband_click/<user_id>', methods = ["POST"])
+def husband_click(user_id):
+    if "user_id" not in session:
+        return redirect('/')
+
+    query = "UPDATE users SET husband = true, wife = false, partnered = false, updated_at = NOW() WHERE id = %(uid)s"
+    data = {'uid':user_id}
+    mysql = connectToMySQL(schema)
+    mysql.query_db(query, data)
+    return redirect('/partners')
+
+@app.route('/partners')
+def partners():
+    if "user_id" not in session:
+        return redirect('/')
+
+    query = "SELECT * from users WHERE partnered = false"
+    mysql = connectToMySQL(schema)
+    users = mysql.query_db(query)
+
+    return render_template("partners.html", users = users)
+
+@app.route('/partner_click/<partner_id>')
+def partner_click(partner_id):
+
+    query = "UPDATE users SET partnered = true, updated_at = NOW() WHERE id = %(sid)s"
+    data = {'sid':session['user_id']}
+    mysql = connectToMySQL(schema)
+    mysql.query_db(query, data)
+
+    query = "UPDATE users SET partnered = true, updated_at = NOW() WHERE id = %(pid)s"
+    data = {'pid':partner_id}
+    mysql = connectToMySQL(schema)
+    mysql.query_db(query, data)
+
+    query = "INSERT INTO partnerships(user_id, partner_id, created_at, updated_at) VALUES (%(sid)s, %(pid)s, NOW(), NOW())"
+    data = {
+        'sid':session['user_id'],
+        'pid':partner_id}
+    mysql = connectToMySQL(schema)
+    mysql.query_db(query,data)
+    return redirect('/account')
+
+@app.route('/account')
+def account():
+    if "user_id" not in session:
+        return redirect('/')
+
+    query="SELECT * from partnerships JOIN users ON partner_id = users.id WHERE user_id = %(sid)s"
+    data = {'sid':session['user_id']}
+    mysql = connectToMySQL(schema)
+    partners = mysql.query_db(query, data)
+    query="SELECT * from partnerships JOIN users ON user_id = users.id WHERE partner_id = %(sid)s"
+    data = {'sid':session['user_id']}
+    mysql = connectToMySQL(schema)
+    partnerships = mysql.query_db(query, data)
+
+    return render_template('account.html', partners = partners, partnerships = partnerships)
+
+@app.route('/partnership/<partnership_id>/<partner_id>')
+def partnership(partnership_id, partner_id):
+    if "user_id" not in session:
+        return redirect('/')
+
+    query="SELECT * from partnerships JOIN users ON partner_id = users.id WHERE user_id = %(sid)s"
+    data = {'sid':session['user_id']}
+    mysql = connectToMySQL(schema)
+    partners = mysql.query_db(query, data)
+    query="SELECT * from partnerships JOIN users ON user_id = users.id WHERE partner_id = %(sid)s"
+    data = {'sid':session['user_id']}
+    mysql = connectToMySQL(schema)
+    partnerships = mysql.query_db(query, data)
+
+    return render_template('partnership.html', partners = partners, partnerships = partnerships)
 
 if __name__ == "__main__":
     app.run(debug=True)
